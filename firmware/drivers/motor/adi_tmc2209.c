eef5f18840832e0cc8b6c36db07073a78583502c
Jacob Martin <jacobm@leaflabs.com>
2024-11-19 11:31:44 -0500

    Fix formatting

--- firmware/drivers/motor/adi_tmc2209.c
+++ firmware/drivers/motor/adi_tmc2209.c
@@ -368,12 +368,13 @@ static int adi_tmc2209_init(const struct device *dev) {
     const int default_hold_delay = 0;  // Set the delay to the minimum to save power.
 
     // Check GSTAT & clear reset
-    struct GSTAT_data_fields gstat_data = { 0 };
+    struct GSTAT_data_fields gstat_data = {0};
     int ret = adi_tmc2209_read(dev, REG_GSTAT, (unsigned char *)&gstat_data);
     if (ret < 0) {
         LOG_ERR("Failed (%d) to read gstat data", ret);
     } else {
-        LOG_DBG("GSTAT flags reset [%d] drv_err [%d] uv_cp [%d]", gstat_data.reset, gstat_data.drv_err, gstat_data.uv_cp);
+        LOG_DBG("GSTAT flags reset [%d] drv_err [%d] uv_cp [%d]", gstat_data.reset, gstat_data.drv_err,
+                gstat_data.uv_cp);
         gstat_data.reset = 1;
         ret = adi_tmc2209_write(dev, REG_GSTAT, (unsigned char *)&gstat_data);
         if (ret < 0) {
@@ -409,7 +410,7 @@ static int adi_tmc2209_init(const struct device *dev) {
     gconf_data.mstep_reg_select = 1;  // MS1 and MS2 are not connected so use UART registers.
     gconf_data.multistep_filt = 1;    // Enable the filter for the multistep pulse. (Done by default.)
 
-    k_sleep(K_MSEC(10)); // I don't know why this works. Are we writing too soon after reading? but it is necessary.
+    k_sleep(K_MSEC(10));  // I don't know why this works. Are we writing too soon after reading? but it is necessary.
     ret = adi_tmc2209_write(dev, REG_GCONF, (unsigned char *)&gconf_data);
 
     if (ret < 0) {
@@ -420,11 +421,13 @@ static int adi_tmc2209_init(const struct device *dev) {
     if (ret < 0) {
         LOG_ERR("Couldn't read GCONF. Defaults may be wrong!");
     } else {
-        LOG_DBG("GCONF data: pdn_disable = %d, internal_Rsense = %d, en_spreadcycle = %d, shaft = %d, i_scale_analog = %d",
-            gconf_data.pdn_disable, gconf_data.internal_Rsense, gconf_data.en_spreadcycle, gconf_data.shaft, gconf_data.i_scale_analog);
+        LOG_DBG(
+            "GCONF data: pdn_disable = %d, internal_Rsense = %d, en_spreadcycle = %d, shaft = %d, i_scale_analog = %d",
+            gconf_data.pdn_disable, gconf_data.internal_Rsense, gconf_data.en_spreadcycle, gconf_data.shaft,
+            gconf_data.i_scale_analog);
     }
 
-    k_sleep(K_MSEC(10)); // added here out of an abundance of caution, see comment above.
+    k_sleep(K_MSEC(10));  // added here out of an abundance of caution, see comment above.
     ret = adi_tmc2209_set_ihold_irun(dev, default_hold_current, default_run_current, default_hold_delay);
 
     return ret;
