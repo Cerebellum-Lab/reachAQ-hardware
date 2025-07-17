8a09f75726de2df0c984eca5cb06ebd9386141f7
Grégory Starck <gregory.starck@toptal.com>
2025-07-17 13:59:29 -0400

    Release GIL for all JerryCan methods + add ReceiveMessages function (#1)
    * Release GIL for all JerryCan methods
    
    Most if not all JC methods are invoking some system call(s) as read/write/etc...
    
    It's highly better to release the GIL during these calls, so that other python threads can get a chance to execute a bit, while these low level system calls are being possibly made.
    
    System calls always invoke some context switch, and sometimes might be blocking, so releasing the GIL in these situations is always better/a win.
    
    * Enh: allow ReceiveMessage during a collect_ms (milisecond) duration
    
    * Prevent spurious EINTR logged in JC.ReceiveMessage, retry if EINTR
    
    * Add JerryCAN::ReceiveMessages
    
    * Restore previous ReceiveMessage as it was, given we have added ReceiveMessages

--- software/libjerrycan/src/libjerrycan.cpp
+++ software/libjerrycan/src/libjerrycan.cpp
@@ -148,8 +148,12 @@ int JerryCAN::SendMessage(const jerrycan_msg_t &msg, const uint16_t dst_id) cons
 int JerryCAN::ReceiveMessage(jerrycan_msg_t &msg) const {
     // Receive a message
     canfd_frame frame = {0};
-    if (read(_can_socket_handle, &frame, sizeof(frame)) <= 0) {
-        if (errno != EAGAIN) {
+    int read_res = read(_can_socket_handle, &frame, sizeof(frame));
+    if (read_res <= 0) {
+        if (read_res == 0) {
+            return -EAGAIN;
+        }
+        if (errno != EAGAIN && errno != EINTR) {
             spdlog::error("Failed to receive CAN message: {}", errno);
         }
         return -errno;
@@ -164,6 +168,37 @@ int JerryCAN::ReceiveMessage(jerrycan_msg_t &msg) const {
     }
 
     return 0;
+}
+
+/* -------------------------------------------------------------------------- */
+
+std::vector<jerrycan_msg_t>
+JerryCAN::ReceiveMessages(
+    unsigned int max_count,
+    unsigned int collect_ms
+) const {
+    jerrycan_msg_t msg;
+    std::vector<jerrycan_msg_t> res_vec;
+    auto end = std::chrono::high_resolution_clock::now() + std::chrono::milliseconds(collect_ms);
+    while (true) {
+        // ReceiveMessage->read is currently blocking for 1 ms when no message immediatelly available.
+        const auto ret = this->ReceiveMessage(msg);  // so no need sleep between calls
+        if (ret == 0) {
+            res_vec.push_back(msg);
+            if (max_count > 0 && res_vec.size() >= max_count) {
+                break;
+            }
+        } else if (ret == -EINTR) {
+            break;
+        } else if (ret != -EAGAIN && ret != -EWOULDBLOCK) {
+            // neither EINTR or EAGAIN, should raise/throw an error instead
+            break;
+        }
+        if (collect_ms == 0 || std::chrono::high_resolution_clock::now() > end) {
+            break;
+        }
+    }
+    return res_vec;
 }
 
 /* -------------------------------------------------------------------------- */
